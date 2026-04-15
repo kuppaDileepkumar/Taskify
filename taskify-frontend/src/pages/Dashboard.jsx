0d82921ea9c8df4a8d031416c307c158ab0efa6d
kuppaDileepkumar <135305572+kuppaDileepkumar@users.noreply.github.com>
2026-04-15 13:38:57 +0530

    Update Dashboard.jsx

--- taskify-frontend/src/pages/Dashboard.jsx
+++ taskify-frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const BASE_URL = "http://localhost:5000/api"
+const BASE_URL = "https://taskify-5shq.onrender.com/api"
 
 export default function Dashboard() {
   const [tasks, setTasks] = useState([])
